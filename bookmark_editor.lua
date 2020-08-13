d840dbc0edf4585e4abb23038d38cc461409dbfd
devin <a18820167281@gmail.com>
2020-08-13 18:04:03 +0800

    1. New Functionality:  add untitled bookmarks. 2. New Functionality:  add autoincrement bookmarks. 3. New Functionality:  sets whether to automatically play the video after adding or jumping to a bookmark.

--- bookmark_editor.lua
+++ bookmark_editor.lua
@@ -15,7 +15,7 @@ dest = nil
 item_name = nil
 
 bookmarks =  {}
-current_item = 1
+current_item = 1 
 
 mode = 0 -- 0 for normal mode ; 1 for edit mode
 
@@ -24,14 +24,14 @@ user_datadir = nil
 
 os_type = nil  -- windows, linux, mac
 
-
 function activate()
 
   if  xpcall(get_media_info,errorHandle)  then
     dbg_print("Welcome")
     check_system()
-    initData()
-    initMainUI()
+    init_data()
+    init_mainUI()
+
   else
 
     local err_dialog = vlc.dialog("warning")
@@ -41,11 +41,10 @@ function activate()
 
 end
 
-function initData()
+function init_data()
+
   get_media_info()
-
   load_preference()
-
   load_bookmarks()
 
 end
@@ -53,7 +52,6 @@ end
 function get_media_info()
 
   input = vlc.object.input()
-
   item_name = vlc.input.item():name()
   
 end
@@ -63,55 +61,96 @@ function load_preference()
   user_datadir = vlc.config.userdatadir()
   pref_path = user_datadir .. '/' .. 'pref' .. ".txt"
 
-  local open_file = can_open_file(pref_path)
+  local can_open_file = can_open_file(pref_path)
   local prefers = {}
-  if (open_file ) then
+
+  if (can_open_file ) then
     for line in io.lines(pref_path) do
       prefers[#prefers + 1] = line
     end
 
     layout_config = prefers[1]
     user_datadir = prefers[2]
+    -- jump_autoplay = prefers[3]
+
   else
-
     xpcall(write_file(pref_path,preference_string()),errorHandle)
-
   end
 
   dest = user_datadir .. '/' .. item_name .. '.txt'
 
 end
 
+--This adds a bookmark with a timestamp only.  --
+function add_bookmark(dec_string)
 
-function capture_bookmark()
-
-  if (mode ==1) then
-    return
-  end
-
-  mode = 1
+  current_item = #bookmarks + 1 
+  bookmarks[current_item] = dec_string
 
   xpcall(vlc_player_pause(),errorHandle)
 
-  current_item = #bookmarks + 1
+end
 
+-- refresh bookmark view and save to hard disk -- 
+function refresh_bookmark()
+
+  refresh_bookmarks_ui()
+  write_bookmarks_to_drive()
+end
+
+
+function capture_bookmark()
+
+  if (mode ==1) then  return     end 
+  mode = 1
+  
   local format_string = current_time_string()
   local dec_string = '[[' .. format_string .. ']]'
-  bookmarks[current_item] = dec_string
 
+  add_bookmark(dec_string)
   change_mode_ui()
 
+
+  update_message("Add the " .. #bookmarks .."th bookmark   ---Press the save button to save")
+
+end
+
+function capture_untitle()
+
+  if (mode ==1) then  return     end 
+
+  local format_string = current_time_string()
+  local dec_string = '[[' .. format_string .. ']]' .. "Untitled"
+
+  add_bookmark(dec_string)
+  refresh_bookmark()
+
+  if (cap_checkbox:get_checked() == true) then  xpcall(vlc_player_resume(),errorHandle) end 
+  update_message("Add the " .. #bookmarks .."th bookmark   ---Press the save button to save")
+end
+
+--use auto-incremented numbers as bookmark--
+function capture_auto()
+
+  if (mode ==1) then  return     end 
+
+  local format_string = current_time_string()
+  local dec_string = '[[' .. format_string .. ']]' .. #bookmarks
+
+  add_bookmark(dec_string)
+  refresh_bookmark()
+
+  if (cap_checkbox:get_checked() == true) then  xpcall(vlc_player_resume(),errorHandle) end 
+  update_message("Add the " .. #bookmarks .."th bookmark   ---Press the save button to save")
 
 end
 
 function save_bookmark()
 
-  if (mode == 0) then
-    return
-  end
+  if (mode == 0) then   return  end
   mode = 0;
 
-  xpcall(vlc_player_resume(),errorHandle)
+  if (cap_checkbox:get_checked() == true) then  xpcall(vlc_player_resume(),errorHandle) end 
 
   update_bookmarks_from_input()
   change_mode_ui()
@@ -122,22 +161,19 @@ end
 function edit_bookmark()
 
   mode = 1
-
   xpcall(vlc_player_pause(),errorHandle)
-
   current_item = selection_item()
   change_mode_ui()
+
+  update_message("Edit " ..current_item .."th bookmark ---" .. bookmarks[current_item] .." ---Press the save button to save")
 
 end
 
 function remove_bookmark()
 
-  if (mode == 1)then
-    return
-  end
+  if (mode == 1)    then    return  end
 
   local selects = bookmarks_list:get_selection()
-  
   local sels = {}
   
   for i,v in pairs(selects) do
@@ -146,14 +182,34 @@ function remove_bookmark()
   
   table.sort(sels, function (o1, o2) return o1 > o2 end )
   
-  for i,v in pairs(sels) do
-  table.remove(bookmarks,v)
-  end
+  for i,v in pairs(sels) do table.remove(bookmarks,v)   end
   
   bookmarks_list:clear()
   show_bookmarks()
   write_bookmarks_to_drive()
 
+  if #sels < 2 then 
+
+    update_message("Remove " .. sels[1] .."th " .."bookmarks")
+
+  else
+    update_message("Remove " .. sels[#sels] .. "th ~ " .. sels[1] .."th " .."bookmarks")
+
+  end 
+
+
+end
+
+function clear_bookmarks()
+
+  if (mode == 1) then return  end 
+  bookmarks = {}
+
+  bookmarks_list:clear()
+  show_bookmarks()
+  write_bookmarks_to_drive()
+
+  update_message("Remove all bookmarks")
 end
 
 function update_bookmarks_from_input()
@@ -161,9 +217,7 @@ function update_bookmarks_from_input()
   local text = text_input:get_text()
 
   if text then
-    if (user_type_nothing(text)) then
-      text = text .. 'Untitled'
-    end
+    if (user_type_nothing(text)) then   text = text .. 'Untitled'   end
     bookmarks[current_item] = text
   end
 
@@ -217,22 +271,30 @@ end
 
 function jump_to_item(item)
 
+  vlc_player_pause()
+
   local line = bookmarks[current_item]
   local format_string = string.gmatch(line, "(%d+:%d+:%d+)")
   for time in format_string do
     stamp = stamp_from_time(time)
     xpcall(jump_to_stamp(stamp),errorHandle)
   end
+
+  update_message("Jump to ".. current_item .. "th bookmark")
+
 end
 
 function jump_to_stamp(stamp)
 
   if stamp then
+
     vlc.var.set(input,"time",stamp)
-    vlc.playlist.play()
+    if jump_checkbox:get_checked() == true then  vlc_player_resume() end 
+
   end
 
 end
+
 
 function edit_with_editor()
 
@@ -246,14 +308,25 @@ function edit_with_editor()
     command_str = string.format('gedit "%s"',dest)
   end
 
+  update_message("Click the reload button to allow the changes edited using the  third editor to take effect. ")
   command_exe(command_str)
+
 
 end
 
 function reload_data()
-  initData()
+
+  init_data()
+
   bookmarks_list:clear()
   show_bookmarks()
+
+  update_message(string.format("Reload %s", dest))
+
+
+
+
+
 end
 
 function update_preference()
@@ -285,9 +358,7 @@ function load_bookmarks()
 
   local can_open = can_open_file(dest)
   if (can_open == true) then
-    for line in io.lines(dest) do
-      bookmarks[#bookmarks+1] = line
-    end
+    for line in io.lines(dest) do   bookmarks[#bookmarks+1] = line  end
   end
 
 end
@@ -296,7 +367,7 @@ function show_bookmarks()
 
   if (bookmarks_list)  then
 
-    if (#bookmarks > 0) then
+    if (bookmarks and #bookmarks > 0) then
 
       for row =1,#bookmarks do
 
@@ -340,7 +411,6 @@ function change_mode_ui()
     end
 
     refresh_bookmarks_ui()
-
   end
 
 end
@@ -353,6 +423,7 @@ function change_layout_ui()
     layout_config = '1'
   else
     layout_config = '0'
+    update_message("switch to vertical mode and restart this extension to take effect")
   end
 
   xpcall(write_file(pref_path,preference_string()),errorHandle)
@@ -368,11 +439,11 @@ function refresh_bookmarks_ui()
 
 end
 
-function initMainUI()
+function init_mainUI()
 
   local layout = frame_with_layout(layout)
 
-  mainUI = vlc.dialog('bookmarks capture')
+  mainUI = vlc.dialog('bookmarks editor')
   local x,y,w,h = layout['top_lay'][1],layout['top_lay'][2],layout['top_lay'][3],layout['top_lay'][4]
   mainUI:add_label('</br>',x,y,w,z)
 
@@ -383,6 +454,7 @@ function initMainUI()
     x,y,w,h = layout['edit_text'][1],layout['edit_text'][2],layout['edit_text'][3],layout['edit_text'][4]
     edit_text = mainUI:add_label('</br>', x,y,w,h)
   end
+
   x,y,w,h = layout['edit_label'][1],layout['edit_label'][2],layout['edit_label'][3],layout['edit_label'][4]
   edit_label_2 = mainUI:add_label('<b style="color:RoyalBlue;">Edit : </b>', x,y,w,h)
 
@@ -398,8 +470,8 @@ function initMainUI()
   x,y,w,h = layout['remove'][1],layout['remove'][2],layout['remove'][3],layout['remove'][4]
   remove_btn = mainUI:add_button('Remove',remove_bookmark,x,y,w,h)
 
-  x,y,w,h = layout['jump_label'][1],layout['jump_label'][2],layout['jump_label'][3],layout['jump_label'][4]
 
+  x,y,w,h = layout['jump_label'][1],layout['jump_label'][2],layout['jump_label'][3],layout['jump_label'][4]
   jump_label = mainUI:add_label('<b style="color:RoyalBlue;">Jump : </b>', x,y,w,h)
 
   x,y,w,h = layout['jump'][1],layout['jump'][2],layout['jump'][3],layout['jump'][4]
@@ -416,8 +488,6 @@ function initMainUI()
 
   x,y,w,h = layout['layout_config'][1],layout['layout_config'][2],layout['layout_config'][3],layout['layout_config'][4]
   
- 
-  
   
   local text = nil
 
@@ -430,6 +500,24 @@ function initMainUI()
   layout_btn = mainUI:add_button(text,change_layout_ui,x,y,w,h)
 
   if (layout_config == '1') then
+
+
+    x,y,w,h = layout['clear'][1],layout['clear'][2],layout['clear'][3],layout['clear'][4]
+    clear_btn = mainUI:add_button('clear',clear_bookmarks,x,y,w,h)
+
+
+    x,y,w,h = layout['cap_untitled'][1],layout['cap_untitled'][2],layout['cap_untitled'][3],layout['cap_untitled'][4]
+    cap_untitle_btn = mainUI:add_button('cap untitle',capture_untitle,x,y,w,h)
+
+
+    x,y,w,h = layout['cap_auto'][1],layout['cap_auto'][2],layout['cap_auto'][3],layout['cap_auto'][4]
+    cap_auto_btn = mainUI:add_button('cap auto',capture_auto,x,y,w,h)
+
+    x,y,w,h = layout['cap_check'][1],layout['cap_check'][2],layout['cap_check'][3],layout['cap_check'][4]
+    cap_checkbox = mainUI:add_check_box("auto play",true, x,y,w,h)
+
+    x,y,w,h = layout['jump_check'][1],layout['jump_check'][2],layout['jump_check'][3],layout['jump_check'][4]
+    jump_checkbox = mainUI:add_check_box("auto play",false, x,y,w,h)
 
     x,y,w,h = layout['location_label'][1],layout['location_label'][2],layout['location_label'][3],layout['location_label'][4]
     mainUI:add_label('<b style="color:RoyalBlue;">Location : </b>', x,y,w,h)
@@ -448,6 +536,14 @@ function initMainUI()
 
     x,y,w,h = layout['go_to'][1],layout['go_to'][2],layout['go_to'][3],layout['go_to'][4]
     mainUI:add_button('Go to',goto_userdir, x,y,w,h)
+
+    x,y,w,h = layout['message'][1],layout['message'][2],layout['message'][3],layout['message'][4]
+    mainUI:add_label('<b style="color:RoyalBlue;">Message : </b>', x,y,w,h)
+
+    x,y,w,h = layout['message_text'][1],layout['message_text'][2],layout['message_text'][3],layout['message_text'][4]
+    message_text = mainUI:add_label('', x,y,w,h)
+
+
     
   else
      x,y,w,h = layout['reload'][1],layout['reload'][2],layout['reload'][3],layout['reload'][4]
@@ -465,9 +561,7 @@ function frame_with_layout(lay)
   if(layout_config == '0' ) then
 
     local y = 4
-    if (os_type == 'mac') then
-      y = 3
-    end
+    if (os_type == 'mac') then   y = 3  end
 
     local frame = {
       top_lay = {1,1,1,0},
@@ -491,40 +585,51 @@ function frame_with_layout(lay)
 
   else
     local y = 6
-    if (os_type == 'mac') then
-      y = 5
-    end
+    if (os_type == 'mac') then  y = 5  end
 
     local frame = {
       top_lay = {1,1,6,1},
-      bookmarks = {1,2,5,3},
-      edit_text = {1,y-2,5,1},
+      bookmarks = {1,2,6,3},
+      edit_text = {1,y-2,6,1},
       edit_label = {1,y-1,1,1},
       capture = {2,y-1,1,1},
       save = {3,y-1,1,1},
       edit = {4,y-1,1,1},
       remove = {5,y-1,1,1},
-      jump_label = {1,y,1,1},
-      jump = {2,y,1,1},
-      jump_text = {3,y,1,1},
-      jump_next = {5,y,1,1},
-      jump_pre = {4,y,1,1},
-      prefer_label = {1,y+1,1,1},
-      layout_config = {2,y+1,1,1},
-      location_label = {1,y+2,1,1},
-      location_text = {2,y+2,2,1},
-      update = {5,y+2,1,1},
-      go_to = {4,y+2,1,1},
-      reload = {4,y+1,1,1},
-      edit_with_editor = {3,y+1,1,1},
-      message = {3,y+2,1,1},
-      message_text = {4,y+2,2,1}
+      clear = {6,y-1,1,1},
+      cap_untitled = {2,y,1,1},
+      cap_auto = {3,y,1,1},
+      cap_check = {5,y,1,1},
+      jump_label = {1,y+1,1,1},
+      jump = {2,y+1,1,1},
+      jump_text = {3,y+1,1,1},
+      jump_next = {4,y+1,1,1},
+      jump_pre = {3,y+1,1,1},
+      jump_check = {5,y+1,1,1},
+      prefer_label = {1,y+2,1,1},
+      layout_config = {2,y+2,1,1},
+      reload = {4,y+2,1,1},
+      edit_with_editor = {3,y+2,1,1},
+      location_label = {1,y+3,1,1},
+      location_text = {2,y+3,3,1},
+      update = {6,y+3,1,1},
+      go_to = {5,y+3,1,1},
+      message = {1,y+4,1,1},
+      message_text = {2,y+4,5,1}
     }
     return frame
 
   end
 
 end
+
+function update_message(contents)
+
+  message_text:set_text("<span style='color:RoyalBlue;opacity:70%;font-size:12px'>"..contents.."</span>")
+
+end 
+
+
 
 function current_location()
 
@@ -536,29 +641,28 @@ function write_bookmarks_to_drive()
 
   local contents = ""
 
-  if (#bookmarks > 0) then
-  
-    for i,v in pairs(bookmarks) do
-      contents = contents .. v .. '\n'
-    end
-  end
-  
-  xpcall(write_file(dest,contents),errorHandle)
+
+    if (bookmarks and #bookmarks > 0) then
+      for i,v in pairs(bookmarks) do  contents = contents .. v .. '\n'  end
+      xpcall(write_file(dest,contents),errorHandle)
+
+  else
+    remove_file(dest)
+  end 
 
 end
 
 function can_open_file(name)
 
   local file = io.open(name,'r')
-
   local exist = false
 
-  if (file ) then
+  if (file) then
     exist = true
     file:close()
   end
-
   return exist
+
 end
 
 function write_file(name,contents)
@@ -570,6 +674,12 @@ function write_file(name,contents)
   end
 
 end
+
+function remove_file(name)
+
+  xpcall(os.remove(name),errorHandle)
+
+end 
 
 
 function user_type_nothing(text)
